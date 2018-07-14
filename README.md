32ceb00a73303a2b0dd12b98f8a3dad2857b2670
gaopeng <priestch@gmail.com>
2018-07-14 10:10:27 +0800

    init README.md with database section

---
+++ README.md
@@ -0,0 +1,16 @@
+### Database
+* start server
+    ```
+    ./bin/db_start.sh
+    ```
+
+* connect database
+    ```
+    psql -h localhost -p 54321 -d luoo
+    ```
+
+* stop server
+    ```
+    ./bin/db_stop.sh
+    ```
+
---
+++ bin/db_stop.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+PROJECT_ROOT=$(dirname $(cd `dirname $0`; pwd))
+DATA_DIR=${PROJECT_ROOT}/pgsql/data
+USER=${1:-luoo}
+DB_NAME=${2:-luoo}
+PORT=${3:-54321}
+DB_HOST=${4:-localhost}
+
+pg_ctl stop -D ${DATA_DIR}
